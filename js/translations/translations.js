e86865db274120d753190ce9425547ad244798f7
marcinzarycki <marcin.zarycki@kineticdata.com>
2016-07-12 16:10:44 -0500

    Added translation console wireframe code

---
+++ js/translations/translations.js
@@ -0,0 +1,35 @@
+/* JS Specific to the Datastore Console */
+(function($, _) {
+    /*----------------------------------------------------------------------------------------------
+     * DOM MANIPULATION AND EVENT REGISTRATION 
+     *   This section is executed on page load to register events and otherwise manipulate the DOM.
+     *--------------------------------------------------------------------------------------------*/
+    $(function() {
+
+        
+        
+    });
+
+    /*----------------------------------------------------------------------------------------------
+     * COMMON INIALIZATION 
+     *   This code is executed when the Javascript file is loaded
+     *--------------------------------------------------------------------------------------------*/
+    // Ensure the BUNDLE global object exists
+    bundle = typeof bundle !== "undefined" ? bundle : {};
+    // Create namespace for Admin Translations Console
+    bundle.adminTranslations = bundle.adminTranslations || {};
+    // Create a scoped alias to simplify references to your namespace
+    var datastore = bundle.adminTranslations;
+
+    /*----------------------------------------------------------------------------------------------
+     * COMMON FUNCTIONS
+     *--------------------------------------------------------------------------------------------*/
+    
+    /**********************************************************************************************
+     ** START *** DATASTORE/CONFIG PAGE *** HELPER METHODS
+     ** Allows for creating of new datastore forms and updating those forms.
+     **********************************************************************************************/
+    
+    
+         
+})($, _);
