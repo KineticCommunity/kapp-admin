60bd394229235613d36bf6cdae25b706380a7375
Norman Orstad <norm.orstad@kineticdata.com>
2017-03-30 12:43:08 -0600

    Reduce dominance of yellow for Notification "variables"

--- js/admin.js
+++ js/admin.js
@@ -1,11 +1,11 @@
 /* Common JS for Admin Kapp */
 (function($, _) {
     /*----------------------------------------------------------------------------------------------
-     * DOM MANIPULATION AND EVENT REGISTRATION 
+     * DOM MANIPULATION AND EVENT REGISTRATION
      *   This section is executed on page load to register events and otherwise manipulate the DOM.
      *--------------------------------------------------------------------------------------------*/
     $(function() {
-        
+
         /**
          * Add aside toggle and remember toggle state.
          */
@@ -14,7 +14,7 @@
             var asideToggle = $("<div>", {class: "aside-toggle"})
                 .on("click", function(){
                     asideSection.closest("div.row").toggleClass("aside-closed");
-                    sessionStorage.setItem("hideAside_" + window.location.pathname, 
+                    sessionStorage.setItem("hideAside_" + window.location.pathname,
                                            asideSection.closest("div.row").hasClass("aside-closed") ? "hide" : null);
                 })
                 .prependTo(asideSection);
@@ -22,7 +22,7 @@
                 asideSection.closest("div.row").addClass("aside-closed");
             }
         }
-        
+
         /**
          * Clear all saved DataTable states if on the home page.
          */
@@ -34,14 +34,14 @@
                 }
             });
         });
-        
+
         admin.tooltip();
         admin.momentify();
         setInterval(admin.momentify, 60000);
     });
 
     /*----------------------------------------------------------------------------------------------
-     * COMMON INIALIZATION 
+     * COMMON INIALIZATION
      *   This code is executed when the Javascript file is loaded
      *--------------------------------------------------------------------------------------------*/
     // Ensure the BUNDLE global object exists
@@ -54,10 +54,10 @@
     /*----------------------------------------------------------------------------------------------
      * COMMON FUNCTIONS
      *--------------------------------------------------------------------------------------------*/
-    
+
     /**
      * Returns an Object with keys/values for each of the url parameters.
-     * 
+     *
      * @returns {Object}
      */
     admin.getUrlParameters = function(param) {
@@ -68,7 +68,7 @@
         }
         return param ? hash[param] : hash;
     };
-    
+
     /**
      * Generates a UUID
      */
@@ -84,7 +84,7 @@
         });
         return uuid;
     };
-    
+
     /**
      * Format date elements with appropriate attribute flags
      * Stores original date in data "date" to prevent issue if this function is run multiple times
@@ -122,12 +122,12 @@
             e.html(moment.duration(diff).humanize());
         });
     }
-    
+
     admin.tooltip = function(container) {
         var dataTooltipElements = container ? container.find("[data-toggle=tooltip]") : $("[data-toggle=tooltip]");
         dataTooltipElements.tooltip();
     }
-    
+
     admin.openAsidePopup = function(){
         (new KD.Modal({
             header: function(element, actions) {
@@ -147,7 +147,7 @@
             renderCallback: false
         })).show();
     };
-    
+
     /**
      * Add jQuery functions for encoding and decoding HTML
      */
@@ -159,11 +159,11 @@
             return $("<div>").html(value).text();
         }
     });
-    
+
     /*----------------------------------------------------------------------------------------------
      * BUNDLE.CONFIG OVERWRITES
      *--------------------------------------------------------------------------------------------*/
-    
+
     /**
      * Overwrite the default field constraint violation error handler to use Notifie to display the errors above the individual fields.
      */
@@ -177,15 +177,15 @@
                 exitEvents: "click"
             };
             if (fieldWrapper.data("notifie-anchor")){
-                notifieOptions.anchor = fieldWrapper.data("notifie-anchor"); 
+                notifieOptions.anchor = fieldWrapper.data("notifie-anchor");
             }
             fieldWrapper.notifie(notifieOptions);
         });
     }
-    
+
     /** Add methods for getting the status and statusClass of a submission **/
     bundle.config.submission = bundle.config.submission || {};
-    
+
     /**
      * Returns the status of the submission, or the core state if status field is not found
      */
@@ -202,14 +202,14 @@
     };
     /**
      * Returns the class used to style the status or state of the submission
-     * The submission parameter object must include: values, form.attributes, and form.kapp.attributes 
+     * The submission parameter object must include: values, form.attributes, and form.kapp.attributes
      */
     bundle.config.submission.getStatusClass = function(submission){
         if (!submission){
             return "label-default";
         }
         var activeStatuses = [], inactiveStatuses = [], cancelledStatuses = [];
-        
+
         if (submission.values && submission.values["Status"]){
             if (submission.form && submission.form.attributes && submission.form.kapp && submission.form.kapp.attributes){
                 var mapFunction = function(attributeName){
@@ -233,7 +233,7 @@
                     cancelledStatuses = $.map(submission.form.kapp.attributes, mapFunction("Statuses - Cancelled"));
                 }
             }
-            
+
             if ($.inArray(submission.values["Status"], activeStatuses) > -1){
                 return "label-success";
             }
@@ -259,11 +259,11 @@
             }
         }
     };
-    
+
     /*----------------------------------------------------------------------------------------------
      * DEFINE RENDERERS FOR DATATABLES FOR VARIOUS FIELD RENDER TYPES
      *--------------------------------------------------------------------------------------------*/
-    
+
     /**
      * Renderer for formatting dates, times, and datetimes.
      */
@@ -299,7 +299,7 @@
             return m.format(type === "sort" || type === "type" ? "x" : to);
         };
     };
-    
+
     /**
      * Renderer for formatting checkboxes.
      */
@@ -325,7 +325,7 @@
             return d;
         };
     };
-    
+
     /**
      * Renderer for formatting attachments.
      */
@@ -347,8 +347,8 @@
                         result += "<br>";
                     }
                     if (type === "display"){
-                        var url = bundle.spaceLocation() 
-                                + "/submissions/" 
+                        var url = bundle.spaceLocation()
+                                + "/submissions/"
                                 + row.ID
                                 + "/files/"
                                 + encodeURIComponent(fieldName)
@@ -362,10 +362,10 @@
                 }
                 return result;
             }
-            return d;   
+            return d;
         };
     };
-    
+
     /**
      * Renderer for formatting text (encode any HTML).
      */
@@ -384,11 +384,11 @@
                 return $.htmlEncode(d);
             }
             else {
-                return d.replace(/\${(.*?)\}/g, " <span style='background-color:yellow'>\$&</span>" )
+                return d.replace(/\${(.*?)\}/g, " <span style='background-color:rgba(255,255,0,0.20)'>\$&</span>" )
             }
         };
     };
-    
+
     /**
      * Function to add renderers to a list of columns with renderType propertiesfor DataTables
      */
@@ -423,12 +423,12 @@
                 case "notificationReplacement":
                     col.render = renderers.notificationReplacement || $.fn.dataTable.render.notificationReplacement();
                     break;
-                default: 
+                default:
                     if (renderers[col.renderType]){
                         col.render = renderers[col.renderType];
                     }
             }
         });
     };
-         
+
 })($, _);
