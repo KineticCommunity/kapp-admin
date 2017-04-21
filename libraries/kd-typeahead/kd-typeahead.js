72eda2fe5f1e791c2523f90437be4c369ad0cae5
marcinzarycki <marcin.zarycki@kineticdata.com>
2017-04-21 09:01:01 -0500

    Updating kd-typeahead

--- libraries/kd-typeahead/kd-typeahead.js
+++ libraries/kd-typeahead/kd-typeahead.js
@@ -1,4 +1,7 @@
 /**
+* KD Typeahead
+* Version 1.0 (2017-04-20)
+* 
 * Prerequisates to using this script are
 * jQuery / underscore / moment
 */
@@ -9,7 +12,7 @@
 * 2. Configure a bridge resource 
 * 3. Create an on load event to call the typeAheadSearch() function OR call the typeAheadSearch() function from within the bundle.config's ready callback in your bundle
 * 4. Add this code to custom head content, or include it within your form's jsp
-* 5. Add the css (at the bottom of this file) to custom head content, or include it within your form's jsp
+* 5. Add the corresponding css (kd-typeahead.css) to custom head content, or include it within your form's jsp
 */
 
 /**
@@ -26,21 +29,20 @@
 /*------------------------------------------------------------
 * TYPEAHEAD SEARCH INPUT ATTRIBTUES: 
 * uses-typeahead               *REQUIRED      (Provide a value if this input uses typeahead ex. 'yes')
-* typeahead-config-object      *OPTIONAL      (Configuration object to use in lieu of the default configuration)
-* typeahead-fields-to-set      *REQUIRED      (Comma separated Field on Form to Bridge Attribute) (e.g. Login Id Field=Login Id,Name Field=Name)
-* typeahead-attribute-to-set   *REQUIRED      (Name of Bridge Attribute to Set in Typeahead Search Field) (e.g. Name)
-* typeahead-attributes-to-show *REQUIRED      (Comma separated List of Bridge Attributes to Show in Typeahead Search) (e.g. Login Id,Name)
 * typeahead-bridged-resource   *REQUIRED      (Name of Bridged Resource to Use - must match a valid bridge resource on the current or shared-resource form)
+* typeahead-attributes-to-show *REQUIRED      (Comma separated List of Bridge Attributes to Show in Typeahead Search) (e.g. Login Id,Name)
+* typeahead-attribute-to-set   *REQUIRED      (Name of Bridge Attribute to Set in Typeahead Search Field) (e.g. Name)
+* typeahead-fields-to-set      *OPTIONAL      (Comma separated Field on Form to Bridge Attribute) (e.g. Login Id Field=Login Id,Name Field=Name)
 * typeahead-bridge-location    *OPTIONAL      (If the search uses a shared-resource form specify it's slug - Defaults to current form)
 * typeahead-query-field        *OPTIONAL      (Name field the Bridged Resource is expecting to be passed as a parameter - Defaults to the typeahead search field)
-* typeahead-fa-class           *OPTIONAL      (Font awesom icon to append to typeahead search and results)
-* typeahead-empy-message       *OPTIONAL      (Message to display if no results are found)
-* typeahead-user-id-attribute  *OPTIONAL      (If filtering results to not include logged in user (for person searching) bridge attribute for the user's Id)
-* typeahead-placeholder        *OPTIONAL      (Placholder text to put into the search field)
-* typeahead-min-length         *OPTIONAL      (Minium Length to begin search)
 * typeahead-additional-params  *OPTIONAL      (Comma separated List of additional values to be provided to the bridge. 
 *                                              The values should be prefixed with STRING:: or FIELD:: depending on if you are passing a 
 *                                              hard coded string, or want the system to get a Field's Value)  (e.g Bridge Param 1=STRING::XYZ,Bridge Param 2=FIELD::FieldName )
+* typeahead-min-length         *OPTIONAL      (Minium Length to begin search)
+* typeahead-fa-class           *OPTIONAL      (Font awesom icon to append to typeahead search and results)
+* typeahead-placeholder        *OPTIONAL      (Placeholder text to put into the search field)
+* typeahead-empy-message       *OPTIONAL      (Message to display if no results are found)
+* typeahead-user-id-attribute  *OPTIONAL      (If filtering results to not include logged in user (for person searching) bridge attribute for the user's username)
 */
 
 (function($, _, moment){
@@ -48,18 +50,13 @@
     // On Load Event that Searches the current form for any "typeahead" input attributes
     typeAheadSearch = function(form){
         
-        $(document).find('input[uses-typeahead]').each(function(){
+        $(form.element()).find('input[uses-typeahead]:not(.typeahead)').each(function(){
             var typeaheadForm = form;
             // Setup Config Variables for Typeahead
             var input = $(this)
             var typeaheadConfig = $.extend({},typeaheadConfigurations['defaultConfiguration'])
             //_.extend(typeaheadConfig, typeaheadConfigurations['defaultConfiguration']) 
             typeaheadConfig['typeaheadForm'] = typeaheadForm;
-            // Check to see if there is a typeahead config object being specified
-            if($(input).attr('typeahead-config-object') !== undefined){
-                // If so, set the typeaheadConfig to the specified config object
-                typeaheadConfig = $.extend(typeaheadConfig, typeaheadConfigurations[$(input).attr('typeahead-config-object')])
-            }
 
             // Override the specified config object if any extra typeahead attributes were set
             if(!_.isEmpty($(input).attr('typeahead-query-field'))) typeaheadConfig['queryField'] = $(input).attr('typeahead-query-field')
@@ -122,9 +119,8 @@
                 })
             }
 
-            // Stop processing if no fields or bridgedresource have been defined 
-            if( Object.keys(typeaheadConfig['fieldsToSet']).length < 1 || _.isEmpty(typeaheadConfig['bridgedResource']) 
-                || _.isEmpty(typeaheadConfig['bridgeUrl']) || _.isEmpty(typeaheadConfig['attrToSet']) ) {
+            // Stop processing if bridgedresource is not defined or attribute to set as value is not defined 
+            if(_.isEmpty(typeaheadConfig['bridgedResource']) || _.isEmpty(typeaheadConfig['bridgeUrl']) || _.isEmpty(typeaheadConfig['attrToSet']) ) {
                 console.log("Typeahead Search Misconfigured");
                 return false;
             }
@@ -183,6 +179,7 @@
                     }
                 }
             );
+            $(input).siblings(".tt-hint").removeAttr("data-element-type");
             
             // Typeahead Change and Select Events
             $(input).bind('typeahead:select', function(ev, suggestion) {
@@ -207,8 +204,8 @@
 // CONFIGURATION Object
 typeaheadConfigurations = {
     defaultConfiguration:{
-        faClass: 'fa-cog',  // Font awesom icon to append to typeahead search and results
-        placeholder: K.translate('bundle','Search by username...'),  // Placholder text to put into the search field
+        faClass: 'fa-search',  // Font awesom icon to append to typeahead search and results
+        placeholder: K.translate('bundle','Start typing to begin your search...'),  // Placholder text to put into the search field
         emptyMessage: K.translate('bundle','No results found'), // Message to display if no results are found
         userIdAttribute : null, // If filtering results to not include logged in user (for person searching) bridge attribute for the user's Id
         queryField:  null, // Name field the Bridged Resource is expecting to be passed as a parameter - Defaults to the typeahead search field
@@ -227,68 +224,14 @@ typeaheadConfigurations = {
                 $(suggestionDetails).append('<div class="tt-attribute col-sm-6">' + data[attr] + '</div>')
             })
             var suggestion = $('<div class="tt-suggestion"/>').append(
-                                $('<i class="fa ' + config['faClass'] + ' fa-3x pull-left" aria-hidden="true"></i>'),
-                                $(suggestionDetails)
-                             );
+                $(suggestionDetails)
+            );
             return suggestion;
         },
         selectedCallback: function(data, config) {  // Data is the row that was selected, all attributes returned from the bridge are available
             // Loop over the Fields to Set and Set them
             $.each( config['fieldsToSet'], function( key, value ) {
-                console.log(key + " " + value);
-                $(config['typeaheadForm'].select('field[' + key + ']').element()).val(data[value]);
-                // Fire change event on field that was set
-                $(config['typeaheadForm'].select('field[' + key + ']').element()).change();
-            });
-        }
-    },
-    // Example for Person Searching
-    personConfiguration:{
-        queryField: 'Username',
-        faClass: 'fa-user',
-        bridgeLocation: 'shared-resources',
-        attrsToShow: ['Username', 'Email', 'Display Name'],
-        bridgedResource: 'Person - By Username',
-        attrToSet: 'Username',
-        fieldsToSet: {
-            "Requested For":"Username",
-            "Requested For Display Name":"Display Name"
-        },
-        selectedCallback: function(data, config){
-            // Loop over the Fields to Set and Set them
-            $.each( config['fieldsToSet'], function( key, value ) {
-                console.log(key + " " + value);
-                $(config['typeaheadForm'].select('field[' + key + ']').element()).val(data[value]);
-                // Fire change event on field that was set
-                $(config['typeaheadForm'].select('field[' + key + ']').element()).change();
-            });
-            // Set the display to the name
-            $('div.requested-for-display-name span.username').text(K('field[Requested For Display Name]').value());
-            // Toggle edit and close
-            $('div.requested-for-display-name span.fa').trigger('click');
-
-        }
-    },
-    // Used by The update ram on existing server (example service)
-    searchForRam:{
-        faClass: 'fa-server',
-        placeholder: 'Start typing to begin your search...',
-        emptyMessage: 'No servers by that name found',
-        queryField:  'Host Name',
-        additionalParams: {"Class":"STRING::BMC_COMPUTERSYSTEM","DataSetId":"STRING::BMC:Asset"},
-        minLength: 2,
-        bridgedResource: 'BMC Computer System',
-        bridgeLocation: null,
-        attrsToShow: ['CI Name', 'Manufacturer', 'Product'],
-        attrToSet: 'CI Name',
-        fieldsToSet: {
-            "Host Name":"CI Name",
-            "Reconciliation Identity":"ReconciliationId"
-        },
-        selectedCallback: function(data, config) {  // Data is the row that was selected, all attributes returned from the bridge are available
-            // Loop over the Fields to Set and Set them
-            $.each( config['fieldsToSet'], function( key, value ) {
-                $(config['typeaheadForm'].select('field[' + key + ']').element()).val(data[value]);
+                config['typeaheadForm'].select('field[' + key + ']').value(data[value]);
                 // Fire change event on field that was set
                 $(config['typeaheadForm'].select('field[' + key + ']').element()).change();
             });
